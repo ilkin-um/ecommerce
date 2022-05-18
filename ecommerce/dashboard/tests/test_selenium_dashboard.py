cb604656ebebd6dae85149eea982cfc256a3d13a
ilkin-um <ilkinum@gmail.com>
2022-05-18 17:22:41 +0400

    added test db fixture

--- ecommerce/dashboard/tests/test_selenium_dashboard.py
+++ ecommerce/dashboard/tests/test_selenium_dashboard.py
@@ -2,7 +2,7 @@ import imp
 import pytest
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+from django.contrib.auth.models import User
 
 @pytest.mark.selenium
 def test_create_test_user(create_super_user):
@@ -11,10 +11,13 @@ def test_create_test_user(create_super_user):
 
 
 @pytest.mark.selenium
-def test_admin_login(live_server,create_super_user,chrome_browser_instance):
+def test_admin_login(live_server,db_fixture_setup,chrome_browser_instance):
     """
     Test login page of Django admin
     """
+
+    user = User.objects.get(id=1)
+    print(user.password)
     browser = chrome_browser_instance
     browser.get(("%s%s" % (live_server.url,"/admin/login/")))
 
--- ecommerce/tests/fixtures.py
+++ ecommerce/tests/fixtures.py
@@ -1,9 +1,27 @@
-import django
-import py
 import pytest
+from django.contrib.auth.models import User
+from django.core.management import call_command
 
 
 @pytest.fixture
 def create_super_user(django_user_model):
+    """
+    Return admin user
+    """
+    return django_user_model.objects.create_superuser(
+        "admin", "", "admin"
+    )
 
-    return django_user_model.objects.create_superuser("admin","","admin")
+
+@pytest.fixture(scope="session")
+def db_fixture_setup(django_db_setup, django_db_blocker):
+    """
+    Load DB data fixtures
+    """
+    with django_db_blocker.unblock():
+        #call_command("loaddata", "db_category_fixture.json")
+        call_command("loaddata", "db_admin_fixture.json")
+
+
+
+
