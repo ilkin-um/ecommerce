567d0d4960acff0281c2b2390596fc90b35fc4ca
ilkin-um <ilkinum@gmail.com>
2022-05-21 12:15:24 +0400

    added fixture test for product table

--- ecommerce/inventory/tests/test_db_fixtures.py
+++ ecommerce/inventory/tests/test_db_fixtures.py
@@ -34,3 +34,40 @@ def test_inventory_db_category_insert(db, category_factory, slug, is_active):
     print(result.name)
     assert result.slug == slug
     assert result.is_active == is_active
+
+
+@pytest.mark.dbfixture
+@pytest.mark.parametrize(
+    "id,web_id,name,slug,description,is_active,created_at,updated_at",
+    [
+        (
+            1,
+            "45425812",
+            "girl primotoo open toe synthetiflip flop sandal",
+            "girl-primotoo-open-toe-synthetiflip-flop-sandal",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin porta, eros vel sollicitudin lacinia, quam metus gravida elit, a elementum nisl neque sit amet orci. Nulla id lorem ac nunc cursus consequat vitae ut orci. In a velit eu justo eleifend tincidunt vel eu turpis. Praesent eu orci egestas, lobortis magna egestas, tincidunt augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aenean vitae lectus eget tortor laoreet efficitur vel et leo. Maecenas volutpat eget ante id tempor. Etiam posuere ex urna, at aliquet risus tempor eu. Aenean a odio odio. Nunc consectetur lorem ante, interdum ultrices elit consectetur sit amet. Vestibulum rutrum interdum nulla. Cras vel mi a enim eleifend blandit. Curabitur ex dui, rutrum et odio sit amet, auctor euismod massa.",
+            "1",
+            "2021-09-04 22:14:18.279096",
+            "2021-09-04 22:14:18.279096",
+        ),
+    ],
+)
+def test_inventory_db_product_dbfixture(
+    db,
+    db_fixture_setup,
+    id,
+    web_id,
+    name,
+    slug,
+    description,
+    is_active,
+    created_at,
+    updated_at,
+):
+    result = models.Product.objects.get(id=id)
+    result_created_at = result.created_at.strftime("%Y-%m-%d %H:%M%:%S")
+
+    assert result.web_id == web_id
+    assert result.name == name
+    assert result.slug == slug
+    assert result_created_at == created_at
